69a8d2c87ebf26de62b310bfd3e85a9884ba220f
Jaume Serrats <jaume.serrats9@gmail.com>
2020-04-29 19:02:38 +0200

    debugging in progress

--- multiple_switch_eth/multiple_switch_eth.ino
+++ multiple_switch_eth/multiple_switch_eth.ino
@@ -8,47 +8,58 @@ Adafruit_MQTT_Client mqtt(&client, "10.0.1.1", 1883);
 Adafruit_MQTT_Publish button = Adafruit_MQTT_Publish(&mqtt, "domos/desk/button");
 
 void setup() {
-  uint8_t mac[6] = {0x7e, 0x1e, 0xc0, 0xba, 0x55, 0x06};
-  Ethernet.begin(mac);
-  for (int i = 2; i < 12; i++) {
-    Serial.println(i);
-    //pinMode(i, INPUT_PULLUP);
+  uint8_t mac[6] = {0x7e, 0x1e, 0xc0, 0xba, 0x55, 0x12};
+    if (Ethernet.begin(mac) == 0) {
+    Serial.println(F("Unable to configure Ethernet using DHCP"));
+    for (;;);
   }
-  pinMode(2, INPUT_PULLUP);
+  for (int i = 3; i < 10; i++) {
+    pinMode(i, INPUT_PULLUP);
+  }
+  MQTT_connect();
   Serial.begin(9600);
   Serial.println("begin");
 }
 
 void loop() {
-  MQTT_connect();
-  for (int i = 2; i < 12; i++) {
-    Serial.println(i);
+  for (int i = 3; i < 10; i++) {
     int sensorVal = digitalRead(i);
+    Serial.print(sensorVal);
     if (sensorVal == HIGH) {
-      Serial.print(i);
-      Serial.print(" HIGH\n");
       //button.publish("high");
     } else {
-      //button.publish("low");
-      Serial.print(i);
-      Serial.print(" LOW\n");
+      MQTT_connect();
+
+      if (!button.publish("test")) {
+        Serial.println(F("Failed"));
+      }
+      else
+      {
+        Serial.println(F("OK!"));
+        delay(400);
+      }
     }
   }
-  delay(1000);
+  Serial.println();
+  delay(100);
 }
 
 void MQTT_connect()
 {
   int8_t ret;
+  Serial.println("trying to connect..");
   if (mqtt.connected())
   {
     return;
+    Serial.println("mqtt connected");
   }
+
   uint8_t retries = 3;
   while ((ret = mqtt.connect()) != 0)
   {
     mqtt.disconnect();
-    delay(5000);
+    Serial.println("mqtt disconnected :(");
+    delay(1000);
     retries--;
     if (retries == 0)
     {
